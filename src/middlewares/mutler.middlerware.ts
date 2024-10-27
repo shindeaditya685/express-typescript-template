0260253fc3490dcd8a4a92d9ead9f056e26ae118
Aditya Shinde <82741617+shindeaditya685@users.noreply.github.com>
2024-10-27 18:15:55 +0530

    Create mutler.middlerware.ts

---
+++ src/middlewares/mutler.middlerware.ts
@@ -0,0 +1,21 @@
+import { Request } from "express";
+import multer, { StorageEngine } from "multer";
+
+const storage: StorageEngine = multer.diskStorage({
+  destination: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, destination: string) => void
+  ) {
+    cb(null, "./public/temp");
+  },
+  filename: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void
+  ) {
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage });
